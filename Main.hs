951755e80e22631bbf8bffd83ac871099bf9168a
Bart Massey <bart@cs.pdx.edu>
2016-03-23 17:16:02 -0700

    reworked flow some for clarity and cleanliness

--- Main.hs
+++ Main.hs
@@ -1,48 +1,44 @@
 module Main where
 
-import Control.Applicative
+import Control.Monad
 import System.Environment
 import System.Exit
 import System.Random
 
+main :: IO ()
 main = getArgs >>= parseArgs
 
 -- The valid characters. The password will be constructed from this pool
 validChars :: String
 validChars = "abcdefghjkmnpqqrstuvxyzABCDEFGHJKLMNPQRSTUVXYZ123456789_-!#=+/"
 
--- Take a String number, read it as an Int and return a random password of that
--- length alongside an ExitCode.
--- Returns one of:
---    (random password, ExitSucces)
---    (help text, ExitFailure)
--- The help text is triggered if 's' cannot be read as an Int.
-generatePassword :: String -> (IO String, ExitCode)
-generatePassword s = case reads s :: [(Int, String)] of
-  [(n, _)] -> (randString n (return ""), ExitSuccess)
-  _ -> (return help, ExitFailure 1)
-
--- Print the help text
+-- The help text
 help :: String
-help = "Usage: passgen OPTION\n" ++
-       "  -l x outputs a random password of x length\n" ++
-       "  -h this help text\n" ++
-       "  -v version of the program"
+help = unlines [ "Usage: passgen OPTION",
+                 "  -l x outputs a random password of x length",
+                 "  -h this help text",
+                 "  -v version of the program" ]
 
 -- Print the String and exit with ExitCode
 outputAndExit :: (String, ExitCode) -> IO ()
 outputAndExit (s,e) = putStrLn s >> exitWith e
 
--- Print the IO String and exit with ExitCode
-outputAndExit' :: (IO String, ExitCode) -> IO ()
-outputAndExit' (s,e) = s >>= putStrLn >> exitWith e
+-- Try to read the given string as a value of the given type.
+maybeRead :: Read a => String -> Maybe a
+maybeRead s =
+    case reads s of
+      [(v, [])] -> Just v
+      _ -> Nothing
 
--- Parse the commandline arguments and return one of:
+-- Parse the commandline arguments and produce:
 --   help
 --   version
 --   actual program output
 parseArgs :: [String] -> IO ()
-parseArgs ("-l":s:[])   = outputAndExit' $ generatePassword s
+parseArgs ["-l", nStr]   =
+    case maybeRead nStr of
+      Just n -> randString n >>= putStrLn
+      Nothing -> outputAndExit (help, ExitFailure 1)
 parseArgs ["-v"]        = outputAndExit (version, ExitSuccess)
 parseArgs ["--version"] = outputAndExit (version, ExitSuccess)
 parseArgs []            = outputAndExit (help, ExitSuccess)
@@ -50,16 +46,12 @@ parseArgs ["-h"]        = outputAndExit (help, ExitSuccess)
 parseArgs ["--help"]    = outputAndExit (help, ExitSuccess)
 parseArgs _             = outputAndExit (help, ExitFailure 1)
 
--- Yields a random char from the validChars list
 randChar :: IO Char
-randChar = (validChars !!) <$> getStdRandom (randomR (0, (length validChars) - 1))
+randChar = (validChars !!) <$> randomRIO (0, length validChars - 1)
 
 -- Generate a 'n' long random IO String.
--- The 's' IO String is appended to the end result.
-randString :: Int -> IO String -> IO String
-randString n s
-  | n < 1 = s
-  | otherwise = randString (n -1) (liftA2 (:) randChar s)
+randString :: Int -> IO String
+randString n = replicateM n randChar
 
 -- Print the version
 version :: String
