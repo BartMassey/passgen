27ede498439b8404a4d7b16aa382941adf74d3ec
Bart Massey <bart@cs.pdx.edu>
2016-03-23 17:48:16 -0700

    converted to ParseArgs

--- Passgen.hs
+++ Passgen.hs
@@ -4,12 +4,43 @@
 import Control.Monad
 import Data.Char
 import Data.List
-import System.Environment
+import System.Console.ParseArgs
 import System.Exit
 import System.Random
+import Text.Printf
+
+data Option = OptionVersion | OptionHelp | OptionLength
+            deriving (Ord, Eq, Show)
+
+argd :: [Arg Option]
+argd = [
+ Arg { argIndex = OptionVersion,
+       argName = Just "version",
+       argAbbr = Just 'v',
+       argData = Nothing,
+       argDesc = "Show program version" },
+ Arg { argIndex = OptionHelp,
+       argName = Just "help",
+       argAbbr = Just 'h',
+       argData = Nothing,
+       argDesc = "Show this usage message" },
+ Arg { argIndex = OptionLength,
+       argName = Just "length",
+       argAbbr = Just 'l',
+       argData = argDataDefaulted "char-count" ArgtypeInt 12,
+       argDesc = "Number of chars in generated password" } ]
 
 main :: IO ()
-main = getArgs >>= parseArgs
+main = do
+  args <- parseArgsIO ArgsComplete argd
+  when (gotArg args OptionVersion) $ do
+         printf "%s: version %s\n" (argsProgName args) version
+         exitSuccess
+  when (gotArg args OptionHelp) $ do
+         putStrLn (argsUsage args)
+         exitSuccess
+  pw <- randString (getRequiredArg args OptionLength)
+  putStrLn pw
 
 -- The valid characters. The password will be constructed from this pool
 validChars :: String
@@ -17,40 +48,6 @@ validChars =
     alphabetics ++ map toUpper alphabetics ++ ['1'..'9'] ++ "_-!#=+/"
     where
       alphabetics = ['a' .. 'z'] \\ ['i', 'o', 'w'] 
-
--- The help text
-help :: String
-help = unlines [ "Usage: passgen OPTION",
-                 "  -l x outputs a random password of x length",
-                 "  -h this help text",
-                 "  -v version of the program" ]
-
--- Print the String and exit with ExitCode
-outputAndExit :: (String, ExitCode) -> IO ()
-outputAndExit (s,e) = putStrLn s >> exitWith e
-
--- Try to read the given string as a value of the given type.
-maybeRead :: Read a => String -> Maybe a
-maybeRead s =
-    case reads s of
-      [(v, [])] -> Just v
-      _ -> Nothing
-
--- Parse the commandline arguments and produce:
---   help
---   version
---   actual program output
-parseArgs :: [String] -> IO ()
-parseArgs ["-l", nStr]   =
-    case maybeRead nStr of
-      Just n -> randString n >>= putStrLn
-      Nothing -> outputAndExit (help, ExitFailure 1)
-parseArgs ["-v"]        = outputAndExit (version, ExitSuccess)
-parseArgs ["--version"] = outputAndExit (version, ExitSuccess)
-parseArgs []            = outputAndExit (help, ExitSuccess)
-parseArgs ["-h"]        = outputAndExit (help, ExitSuccess)
-parseArgs ["--help"]    = outputAndExit (help, ExitSuccess)
-parseArgs _             = outputAndExit (help, ExitFailure 1)
 
 -- Generate a random valid Char.
 randChar :: IO Char
